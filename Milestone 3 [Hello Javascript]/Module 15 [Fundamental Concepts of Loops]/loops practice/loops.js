c88049b0c17f18426d3b0dab13320747b489b074
kohinurfatema <kohinurfatema35@gmail.com>
2025-07-30 04:44:33 -0700

    loop practice

---
+++ Milestone 3 [Hello Javascript]/Module 15 [Fundamental Concepts of Loops]/loops practice/loops.js
@@ -0,0 +1,78 @@
+
+
+
+//!for loop task1 sum of odd numbers//
+
+let sumodd =0;
+for (let i=91; i<=129; i++)
+   if(i%2 !== 0){
+    sumodd +=i;
+    console.log("this odd:",i);
+   }
+   console .log("final sum of odds:", sumodd);
+
+
+
+//!for loop task2 countdown//
+
+for (let i=81; i>=65; i--){
+    console.log(i);
+}
+
+
+//!for loop task3 commitment//
+
+for (let i=1; i<=60; i++){
+    console.log("I will invest at least 6 hours every singel day for next 60 days");
+}
+
+//!for loop task4 sum of even numbers//
+
+let sum =0;
+for (let i=51; i<=85; i++){
+   if (i%2 === 0){
+    sum +=i;
+    console.log("this even:",i);
+   }
+}
+console.log("sum of even numbers:",sum);
+
+
+
+//!while loop task1//
+
+let num =61;
+while(num<=100){
+    if (num%2 !==0){
+        console.log("odd number",num)
+    }
+    num++;
+}
+
+
+//!while loop commitment//
+
+let count =1;
+while(count<=60){
+    console.log("I will invest 6 hours every day")
+    count++
+}
+
+
+//!while loop even numbers//
+
+let even=206;
+while(even<=311){
+    if (even%2 ===0){
+        console.log("even nember:",even)
+    }
+    even++
+}
+
+//!while loop countdown//
+
+let countdown=21;
+while(countdown>=15){
+    console.log(countdown);
+    countdown--
+}
