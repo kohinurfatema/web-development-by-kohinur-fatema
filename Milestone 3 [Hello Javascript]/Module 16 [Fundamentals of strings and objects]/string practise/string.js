b3103c6edb7e144947f23972a4d2d096ddcf950d
kohinurfatema <kohinurfatema35@gmail.com>
2025-08-02 10:23:10 -0700

    string practice

---
+++ Milestone 3 [Hello Javascript]/Module 16 [Fundamentals of strings and objects]/string practise/string.js
@@ -0,0 +1,39 @@
+
+
+
+//task1//
+
+const name ="banana";
+const count = name.split("a").length -1;
+console.log(count);
+
+
+//task2//
+
+const str ="Apple And bananas";
+const smallA =str.split("a").length -1;
+const capitalA =str.split("A").length -1;
+
+const total =smallA+capitalA;
+console.log(total);
+
+//task3//
+
+let vowel = "education";
+vowel =vowel.toLowerCase();
+
+let hasAllVowel =
+             vowel.includes("a") &&
+             vowel.includes("e") &&
+             vowel.includes("i") &&
+             vowel.includes("o") &&
+             vowel.includes("u");
+
+console.log(hasAllVowel);
+
+
+//task4//
+
+let word ="exam";
+let newWord = word.replace("x","y");
+console.log(newWord);
