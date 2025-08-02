d141c05b0c65c4cd41f92ce39d0eb979deca3ada
kohinurfatema <kohinurfatema35@gmail.com>
2025-08-02 04:05:16 -0700

    task5 solving

--- Milestone 3 [Hello Javascript]/Module 16 [Fundamentals of strings and objects]/objects practice/objects.js
+++ Milestone 3 [Hello Javascript]/Module 16 [Fundamentals of strings and objects]/objects practice/objects.js
@@ -41,3 +41,31 @@ const student = {
     }
 };
 console.log(student.physics.marks);
+
+
+//task4//
+
+let citizen = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+const count = Object.keys(citizen).length;
+console.log(count);
+
+
+//task5//
+
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+
+};
+
+for (let key in myObject){
+    console.log("key:"+key + "type:"+ typeof myObject [key]);
+}
