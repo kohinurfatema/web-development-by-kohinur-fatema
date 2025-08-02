16a953cf5d695b806fa950e297ebb336be363c1f
kohinurfatema <kohinurfatema35@gmail.com>
2025-08-02 04:17:36 -0700

    last object practice

--- Milestone 3 [Hello Javascript]/Module 16 [Fundamentals of strings and objects]/objects practice/objects.js
+++ Milestone 3 [Hello Javascript]/Module 16 [Fundamentals of strings and objects]/objects practice/objects.js
@@ -67,5 +67,5 @@ let myObject = {
 };
 
 for (let key in myObject){
-    console.log("key:"+key + "type:"+ typeof myObject [key]);
+    console.log("key:"+key +"/"+"type:"+ typeof myObject [key]);
 }
