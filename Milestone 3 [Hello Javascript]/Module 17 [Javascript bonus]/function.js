f81ccfe90eacd5ce24ac2c1c724adadeffcf4545
kohinurfatema <kohinurfatema35@gmail.com>
2025-08-04 09:20:17 -0700

    first function

---
+++ Milestone 3 [Hello Javascript]/Module 17 [Javascript bonus]/function.js
@@ -0,0 +1,37 @@
+function plusMachine(input1,input2){
+    let sum = input1 + input2;
+    console.log("sum is = " + sum);
+    return sum;
+}
+const plus = plusMachine(40,10);
+console.log(plus);
+
+
+function minusMachine(input1,input2){
+    let sum = input1-input2;
+    console.log("sum is = " + sum);
+    return sum;
+}
+
+const result = minusMachine(40,10);
+console.log(result);
+
+
+
+function multiMachine(input1,input2){
+    let sum = input1*input2;
+    console.log("sum is = "+ sum);
+    return sum;
+}
+const ans = multiMachine(40,10);
+console.log(ans);
+
+
+
+function divideMachine(input1,input2){
+    let sum = input1/input2;
+    console.log("sum is = " + sum);
+    return sum;
+}
+const divide = divideMachine(40,10);
+console.log(divide);
